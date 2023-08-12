1ddd34f9cb75fca4901707980c9b9de185f06524
sujith-rek <ps335@snu.edu.in>
2023-08-12 12:19:55 +0530

    create paths

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ShopService } from './services/shop/shop.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    private readonly shopService: ShopService
+    ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('/post_payment')
+  postPayment(): Promise<String | null> {
+    const data = {
+      "amount": 100,
+      "currency": "NGN",
+    }
+    return this.shopService.postPayment(data);
+  }
+
+  @Post('/verify_payment')
+  verifyPayment(): Promise<String | null> {
+    const data = {
+      "amount": 100,
+      "currency": "NGN",
+    }
+    return this.shopService.verifyPayment(data);
+  }
+
 }
--- src/services/shop/shop.service.ts
+++ src/services/shop/shop.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ShopService {}
+export class ShopService {
+    constructor(private prisma: PrismaService) {}
+
+    postPayment(data: any) : Promise<String | null>{
+        const str = "Meow"
+        return new Promise((resolve, reject) => {
+            resolve(str)
+        }
+        );
+    }
+
+    verifyPayment(data: any) : Promise<String | null>{
+        const str = "Meow"
+        return new Promise((resolve, reject) => {
+            resolve(str)
+        }
+        );
+    }
+
+    
+
+}
